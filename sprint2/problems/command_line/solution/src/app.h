dffe959e563f6f267039e5579356725cfbbfdead
ArtemBystrovOfficial <bystrov.official.one@gmail.com>
2024-02-08 11:58:53 -0800

    feat: command_line full

---
+++ sprint2/problems/command_line/solution/src/app.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "players.h"
+
+namespace app {
+
+// Facade
+class App {
+   public:
+    App(const std::filesystem::path& settings_json);
+
+    const Players& GetPlayers() const { return players_; }
+    Players& GetMutablePlayers() { return players_; }
+
+    const model::Game& GetGame() const { return game_; }
+    model::Game& GetMutableGame() { return game_; }
+
+    void SetTickEditAccess(bool is_open) { tick_edit_access_ = is_open; }
+    bool GetTickEditAccess() { return tick_edit_access_; }
+
+   private:
+    model::Game game_;
+    Players players_;
+
+    bool tick_edit_access_;
+};
+
+}  // namespace app
