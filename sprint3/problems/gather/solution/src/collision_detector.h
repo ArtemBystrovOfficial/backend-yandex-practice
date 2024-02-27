82682e6ae39aac9d74c7b0bf568539abde10e7b3
ArtemBystrovOfficial <bystrov.official.one@gmail.com>
2024-02-27 07:44:02 -0800

    feat: gather solution

---
+++ sprint3/problems/gather/solution/src/collision_detector.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+namespace geom {
+    struct Point2D {
+        double x,y;
+    };
+    struct Rect {
+        Point2D left_down, right_up;
+    };
+}
+
+namespace collision_detector {
+
+struct Item {
+    geom::Point2D position;
+    double width;
+};
+
+struct Gatherer {
+    geom::Point2D start_pos;
+    geom::Point2D end_pos;
+    double width;
+};
+
+class ItemGathererProvider {
+protected:
+    ~ItemGathererProvider() = default;
+
+public:
+    virtual size_t ItemsCount() const = 0;
+    virtual Item GetItem(size_t idx) const = 0;
+    virtual size_t GatherersCount() const = 0;
+    virtual Gatherer GetGatherer(size_t idx) const = 0;
+};
+
+struct GatheringEvent {
+    size_t item_id;
+    size_t gatherer_id;
+    double sq_distance;
+    double time;
+};
+
+std::vector<GatheringEvent> FindGatherEvents(const ItemGathererProvider& provider);
+
+}  // namespace collision_detector
